a95e3e0d16d1ffe3f3cb2aa38e28d132e9bc7533
Jennifer Qiao <qiaojennifer97@gmail.com>
2022-09-13 16:02:05 +0100

    remote db commected to server, completed

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import { Client } from "pg";
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
+import { incrementVoteByBreedName, doesBreedNameExists, newBreedRow  } from "./db";
 
 config(); //Read .env file lines as though they were env vars.
 
@@ -15,7 +16,7 @@ const herokuSSLSetting = { rejectUnauthorized: false }
 const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
-  ssl: sslSetting,
+  ssl: { rejectUnauthorized: false },
 };
 
 const app = express();
@@ -26,10 +27,23 @@ app.use(cors()) //add CORS support to each following route handler
 const client = new Client(dbConfig);
 client.connect();
 
-app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
-  res.json(dbres.rows);
+
+
+app.get("/leaderboard", async (req, res) => {
+  const dbres = await client.query('select * from votes ORDER BY votes DESC LIMIT 10');
+  res.status(200).json(dbres.rows);
 });
+
+app.post<{}, {}, {breedName: string}>("/votes", async (req,res)=>{
+  const {breedName} = req.body;
+  if (await doesBreedNameExists(client,breedName)) {
+   const voteNumber = await incrementVoteByBreedName(client, breedName)
+    res.status(200).json({votes: voteNumber})
+  } else {
+  const newBreedRowVote = await newBreedRow(client, breedName)
+    res.status(200).json({votes: newBreedRowVote})
+  }
+}) 
 
 
 //Start the server on the given port
@@ -40,3 +54,4 @@ if (!port) {
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
 });
+
